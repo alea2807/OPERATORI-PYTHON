69b5250dc91d32173545bae5eeb0338ec2d20798
alexandra605 <70931822+alexandra605@users.noreply.github.com>
2020-09-22 15:34:01 +0300

    Add files via upload

---
+++ Problema 6.py
@@ -0,0 +1,13 @@
+n=1866
+a=n%1000%100%10//1
+print(a)
+b=n%1000%100//10
+print(b)
+c=n%1000//100
+print(c)
+d=n//1000
+print(d)
+print("suma","=",a+b+c+d)
+print("d","c","b","a")
+print(n//9)
+print(n%9)
